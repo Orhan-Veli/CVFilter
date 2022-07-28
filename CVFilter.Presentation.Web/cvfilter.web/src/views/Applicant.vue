4f574dcde2d5f175be99b55d209599e441319fab
Orhan-Veli <orhan_1576@hotmail.com>
2022-07-29 00:36:43 +0300

    Application Page Css Fixed

--- CVFilter.Presentation.Web/cvfilter.web/src/views/Applicant.vue
+++ CVFilter.Presentation.Web/cvfilter.web/src/views/Applicant.vue
@@ -1,245 +1,69 @@
 <template>
-    <table id="dt-basic-checkbox" class="table" cellspacing="0" width="100%">
-  <thead>
-    <tr>
-      <th></th>
-      <th class="th-sm">Position
-      </th>
-      <th class="th-sm">Office
-      </th>
-      <th class="th-sm">Age
-      </th>
-      <th class="th-sm">Start date
-      </th>
-      <th class="th-sm">Salary
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td></td>
-      <td>System Architect</td>
-      <td>Edinburgh</td>
-      <td>61</td>
-      <td>2011/04/25</td>
-      <td>$320,800</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Accountant</td>
-      <td>Tokyo</td>
-      <td>63</td>
-      <td>2011/07/25</td>
-      <td>$170,750</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Junior Technical Author</td>
-      <td>San Francisco</td>
-      <td>66</td>
-      <td>2009/01/12</td>
-      <td>$86,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Senior Javascript Developer</td>
-      <td>Edinburgh</td>
-      <td>22</td>
-      <td>2012/03/29</td>
-      <td>$433,060</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Accountant</td>
-      <td>Tokyo</td>
-      <td>33</td>
-      <td>2008/11/28</td>
-      <td>$162,700</td>
-    </tr>
-    <tr>
-      <td></td>
-
-      <td>Integration Specialist</td>
-      <td>New York</td>
-      <td>61</td>
-      <td>2012/12/02</td>
-      <td>$372,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Sales Assistant</td>
-      <td>San Francisco</td>
-      <td>59</td>
-      <td>2012/08/06</td>
-      <td>$137,500</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Integration Specialist</td>
-      <td>Tokyo</td>
-      <td>55</td>
-      <td>2010/10/14</td>
-      <td>$327,900</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Javascript Developer</td>
-      <td>San Francisco</td>
-      <td>39</td>
-      <td>2009/09/15</td>
-      <td>$205,500</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Software Engineer</td>
-      <td>Edinburgh</td>
-      <td>23</td>
-      <td>2008/12/13</td>
-      <td>$103,600</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Office Manager</td>
-      <td>London</td>
-      <td>30</td>
-      <td>2008/12/19</td>
-      <td>$90,560</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Support Lead</td>
-      <td>Edinburgh</td>
-      <td>22</td>
-      <td>2013/03/03</td>
-      <td>$342,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Regional Director</td>
-      <td>San Francisco</td>
-      <td>36</td>
-      <td>2008/10/16</td>
-      <td>$470,600</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Senior Marketing Designer</td>
-      <td>London</td>
-      <td>43</td>
-      <td>2012/12/18</td>
-      <td>$313,500</td>
-    </tr>
-    <tr>
-      <td></td>
-
-      <td>Regional Director</td>
-      <td>London</td>
-      <td>19</td>
-      <td>2010/03/17</td>
-      <td>$385,750</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Marketing Designer</td>
-      <td>London</td>
-      <td>66</td>
-      <td>2012/11/27</td>
-      <td>$198,500</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Chief Financial Officer (CFO)</td>
-      <td>New York</td>
-      <td>64</td>
-      <td>2010/06/09</td>
-      <td>$725,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Systems Administrator</td>
-      <td>New York</td>
-      <td>59</td>
-      <td>2009/04/10</td>
-      <td>$237,500</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Software Engineer</td>
-      <td>London</td>
-      <td>41</td>
-      <td>2012/10/13</td>
-      <td>$132,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Personnel Lead</td>
-      <td>Edinburgh</td>
-      <td>35</td>
-      <td>2012/09/26</td>
-      <td>$217,500</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Development Lead</td>
-      <td>New York</td>
-      <td>30</td>
-      <td>2011/09/03</td>
-      <td>$345,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Chief Marketing Officer (CMO)</td>
-      <td>New York</td>
-      <td>40</td>
-      <td>2009/06/25</td>
-      <td>$675,000</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Pre-Sales Support</td>
-      <td>New York</td>
-      <td>21</td>
-      <td>2011/12/12</td>
-      <td>$106,450</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Sales Assistant</td>
-      <td>Sidney</td>
-      <td>23</td>
-      <td>2010/09/20</td>
-      <td>$85,600</td>
-    </tr>
-    <tr>
-      <td></td>
-      <td>Chief Executive Officer (CEO)</td>
-      <td>London</td>
-      <td>47</td>
-      <td>2009/10/09</td>
-      <td>$1,200,000</td>
-    </tr>
-  </tbody>
-  <tfoot>
-    <tr>
-      <th>
-      </th>
-      <th>Position
-      </th>
-      <th>Office
-      </th>
-      <th>Age
-      </th>
-      <th>Start date
-      </th>
-      <th>Salary
-      </th>
-    </tr>
-  </tfoot>
-</table>
+  <div class="container">
+    	<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Applicant</h3>
+						<div class="pull-right">
+							<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+								<i class="glyphicon glyphicon-filter"></i>
+							</span>
+						</div>
+					</div>
+					<div class="panel-body">
+						<input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
+					</div>
+					<table class="table table-hover" id="dev-table">
+						<thead>
+							<tr>
+								<th>Id</th>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Phone</th>
+                <th>Matches</th>
+                <th>Language</th>
+                <th>School</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Kilgore</td>
+								<td>Trout</td>
+								<td>kilgore</td>
+                <td>kilgore</td>
+                <td>kilgore</td>
+                <td>kilgore</td>
+							</tr>
+              <tr>
+								<td>1</td>
+								<td>Kilgore</td>
+								<td>Trout</td>
+								<td>kilgore</td>
+                <td>kilgore</td>
+                <td>kilgore</td>
+                <td>kilgore</td>
+							</tr>
+              <tr>
+								<td>1</td>
+								<td>Kilgore</td>
+								<td>Trout</td>
+								<td>kilgore</td>
+                <td>kilgore</td>
+                <td>kilgore</td>
+                <td>kilgore</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-    name: "Applicant",
+    name: "Create",
     data(){
         return{
 
@@ -255,4 +79,33 @@ export default {
 }
 </script>
 <style>
+	.row{
+		    margin-top:40px;
+		    padding: 0 10px;
+		}
+		.clickable{
+		    cursor: pointer;   
+		}
+
+		.panel-heading div {
+			margin-top: -18px;
+			font-size: 15px;
+		}
+		.panel-heading div span{
+			margin-left:5px;
+		}
+		.panel-body{
+			display: none;
+		}
+    .panel-title{
+      align-items: center;
+    }
+    .container{
+      margin-left: 30%;
+      margin-top: 5%;
+      border: 10px solid #222;
+      width: 40%;
+      border-radius: 50px;
+      background: white;
+    }
 </style>
